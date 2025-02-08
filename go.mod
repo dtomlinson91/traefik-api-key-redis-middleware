f05082dc0441208bada4d3f51d2f789b2c904e23
Daniel Tomlinson <dan.tomlinson@agentsoftware.net>
2025-02-08 00:43:25 +0000

    remove radix

--- go.mod
+++ go.mod
@@ -1,10 +1,3 @@
 module github.com/dtomlinson91/traefik-api-key-redis-middleware
 
 go 1.19
-
-require github.com/mediocregopher/radix/v3 v3.8.1
-
-require (
-	github.com/stretchr/testify v1.5.1 // indirect
-	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 // indirect
-)
--- plugin.go
+++ plugin.go
@@ -1,13 +1,15 @@
 package traefik_api_key_redis_middleware
 
 import (
+	"bufio"
 	"context"
+	"encoding/json"
+	"fmt"
+	"net"
 	"net/http"
 	"regexp"
 	"strings"
 	"sync"
-
-	"github.com/mediocregopher/radix/v3"
 )
 
 type Config struct {
@@ -25,29 +27,59 @@ func CreateConfig() *Config {
 
 type ApiKeyRedis struct {
 	next        http.Handler
-	redisPool   radix.Client
+	redisHost   string
 	cache       map[string]string
 	cacheMutex  sync.RWMutex
 	bearerRegex *regexp.Regexp
 }
 
-func createRedisPool(redisURL string) radix.Client {
-	client, err := radix.NewPool("tcp", redisURL, 10)
+func getKeyFromRedis(inst string, key string) (string, error) {
+	// create connection
+	conn, err := net.Dial("tcp", inst)
 	if err != nil {
-		panic(err)
+		return "nil", fmt.Errorf("failed to connect to Redis: %v", err)
 	}
-	return client
+	defer conn.Close()
+
+	// send command to Redis
+	cmd := fmt.Sprintf("*2\r\n$3\r\nGET\r\n$%d\r\n%s\r\n", len(key), key)
+	_, err = conn.Write([]byte(cmd))
+	if err != nil {
+		return "", fmt.Errorf("failed to write to Redis: %v", err)
+	}
+
+	// read response
+	reader := bufio.NewReader(conn)
+	resp, err := reader.ReadString('\n')
+	if err != nil {
+		return "", fmt.Errorf("failed to read from Redis: %v", err)
+	}
+
+	// check if key not found
+	if resp == "$-1\r\n" {
+		return "", fmt.Errorf("key not found")
+	}
+
+	// check for value
+	if resp[0] == '$' {
+		value, err := reader.ReadString('\n')
+		if err != nil {
+			return "", fmt.Errorf("failed to read from Redis: %v", err)
+		}
+		return value[:len(value)-2], nil
+	}
+
+	return "", fmt.Errorf("failed to get value from Redis %s", resp)
 }
 
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	if config.RedisHost == nil {
 	}
 	redisHost := "kraken-api-redis.kraken-api.svc.data-applications:6379"
-	pool := createRedisPool(redisHost)
 
 	return &ApiKeyRedis{
 		next:        next,
-		redisPool:   pool,
+		redisHost:   redisHost,
 		cache:       make(map[string]string),
 		bearerRegex: regexp.MustCompile(`^Bearer\s+(.+)$`),
 	}, nil
@@ -66,7 +98,11 @@ func (a *ApiKeyRedis) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 	if apiHeader != "" {
 		if _, exists := a.cache[apiHeader]; exists {
-			a.next.ServeHTTP(rw, req)
+			// a.next.ServeHTTP(rw, req)
+			response := Response{Message: "x-api-key is in cache", StatusCode: http.StatusOK}
+			rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+			rw.WriteHeader(response.StatusCode)
+			json.NewEncoder(rw).Encode(response)
 			return
 		}
 	}
@@ -76,7 +112,11 @@ func (a *ApiKeyRedis) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		if len(bearerMatches) == 2 {
 			bearerToken = bearerMatches[1]
 			if _, exists := a.cache[bearerToken]; exists {
-				a.next.ServeHTTP(rw, req)
+				// a.next.ServeHTTP(rw, req)
+				response := Response{Message: "bearer token is in cache", StatusCode: http.StatusOK}
+				rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+				rw.WriteHeader(response.StatusCode)
+				json.NewEncoder(rw).Encode(response)
 				return
 			}
 		} else {
@@ -90,15 +130,18 @@ func (a *ApiKeyRedis) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		apiToken = bearerToken
 	}
 
-	var rkey string
-	if err := a.redisPool.Do(radix.Cmd(&rkey, "GET", apiToken)); err != nil {
-		http.Error(rw, "API key is not valid", http.StatusUnauthorized)
-		return
+	val, err := getKeyFromRedis(a.redisHost, apiToken)
+	if err != nil {
+		http.Error(rw, fmt.Sprintf("Error getting key: %v", err), http.StatusUnauthorized)
 	}
 
 	a.cacheMutex.Lock()
-	a.cache[apiToken] = rkey
+	a.cache[apiToken] = val
 	a.cacheMutex.Unlock()
 
-	a.next.ServeHTTP(rw, req)
+	// a.next.ServeHTTP(rw, req)
+	response := Response{Message: "Got key from redis and saved to cache", StatusCode: http.StatusOK}
+	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+	rw.WriteHeader(response.StatusCode)
+	json.NewEncoder(rw).Encode(response)
 }
